a04239b4c1add753f5da118273054b50e2d491ed
jicode1 <87397423+jicode1@users.noreply.github.com>
2022-02-01 01:39:10 -0300

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,16 @@
+let nivelPoder = prompt('Inserte Nivel de Poder del 1 al 100');
+
+if (nivelPoder <= 20) {
+    alert('Shinobu: "Baka 😖" ');
+} else if (nivelPoder <= 40) {
+    alert('Mitsuri: "El amor es lo mejor 😍" ');
+} else if (nivelPoder <= 60) {
+    alert('Rengoku: "Soy una dona 🍩" ');
+} else if (nivelPoder <= 80) {
+    alert('Tomioka: "No me odia nadie 😎" ');
+} else if (nivelPoder <= 100) {
+    alert('Sanemi: "Un demonio se comio a la que me gusta 😞" ');
+} else {
+
+    alert('Inserta un número del 1 al 100 pelotudo');
+}
